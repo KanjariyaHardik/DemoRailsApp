1f2a4a803d7a1628f823dff869e893efa8134eb3
Hardik Kanjariya <hardik@bigkittylabs.com>
2022-05-04 12:25:28 +0530

    Devise Setup for User Module

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  first_name             :string           not null
+#  last_name              :string
+#  age                    :string
+#  gender                 :string
+#  phone                  :string
+#  role                   :integer          default("user")
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+
+  ## ENUM
+  enum role: {user: 0, admin: 1}
+
+  ## VALIDATONS
+  validates :first_name, presence: true
+end
